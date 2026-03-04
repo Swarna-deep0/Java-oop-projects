b65ac73da44b61f470511be357419ad47e6fa554
Swarna-deep0 <srj368920@gmail.com>
2026-03-04 20:56:55 +0530

    DSA LEVEL 1

---
+++ src/com/DSA.java
@@ -0,0 +1,50 @@
+package com;
+
+public class DSA {
+    public static void main(String[] args) {
+        // Number pattern problem :
+        pattern(6);
+
+        // parallel star pattern :
+        pattern2(3);
+
+        // numbern pattern :
+        pattern3(5);
+ 
+        // row wise number pattern print :
+        pattern4(4);
+    }
+    public static void pattern(int n){
+        for (int i = 1 ; i<=n ; i++){
+            for (int j = 1 ; j<=n ; j++){
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern2(int n){
+        for (int i = 1 ; i<=n ; i++){
+            for (int j = 1 ; j<=n ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern3(int n){
+        for (int i = 1 ; i<=n ; i++){
+            for (int j = 1 ; j<=n ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern4(int n){
+        for (int i = 1 ; i<=n ; i++){
+            for (int j = 1 ; j<=n ; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
